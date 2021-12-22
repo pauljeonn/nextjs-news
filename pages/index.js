0a1798e520817447f034d5100d3d3e5f0f7ccea0
Paul Jeon <pauljeonn@gmail.com>
2021-12-23 08:42:42 +0900

    create Navbar component

--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,13 @@
-import Head from 'next/head';
-import Image from 'next/image';
+import Navbar from '../components/Navbar';
 import styles from '../styles/Home.module.css';
 
 export default function Home() {
-	return <>News App</>;
+	return (
+		<div className="container">
+			<Navbar />
+			<div className={styles.main}>
+				<h1>News App</h1>
+			</div>
+		</div>
+	);
 }
